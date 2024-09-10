1a5f2ace82ebf0038e4fa12b9bef8b38a3862cf0
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-09-10 21:24:34 +0900

    コードミスを修正

--- src/Assets/Scripts/Feature/Presenter/PlayerPresenter.cs
+++ src/Assets/Scripts/Feature/Presenter/PlayerPresenter.cs
@@ -33,7 +33,7 @@ namespace Feature.Presenter
         public PlayerPresenter(
             PlayerModel model,
             CharacterParams characterParams,
-            VoltageBar voltageBar
+            VoltageBar voltageBar,
             GameUIView ui
 
         )
