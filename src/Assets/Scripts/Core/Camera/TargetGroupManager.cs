543a7c3ac3c36de1bbf97fe568326eb1a0df8683
yuhi yamane <bird9.yuhi@gmail.com>
2024-10-05 11:42:18 +0900

    :bento: chore: 🎯 距離ベースでターゲット管理ロジックを改善
    Updateメソッドとターゲット管理メソッドをリファクタリングしました。内部リストでのターゲット状態の追跡を追加。

--- src/Assets/Scripts/Core/Camera/TargetGroupManager.cs
+++ src/Assets/Scripts/Core/Camera/TargetGroupManager.cs
@@ -1,6 +1,5 @@
 #region
 
-using System;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
@@ -12,22 +11,43 @@ namespace Core.Camera
     [RequireComponent(typeof(CinemachineTargetGroup))]
     public class TargetGroupManager : MonoBehaviour
     {
-        private CinemachineTargetGroup targetGroup;
+        [SerializeField] private float objectDistanceThreshold = 20f;
+
+        private float lastCheckAt;
 
         private List<(Transform, CameraTargetGroupTag)> objects;
 
         private GameObject playerForwardObject;
 
         private Transform playerTransform;
+        private CinemachineTargetGroup targetGroup;
 
         private void Awake()
         {
+            objects = new();
             targetGroup = GetComponent<CinemachineTargetGroup>();
         }
 
         private void Update()
         {
-            
+            if (Time.time - lastCheckAt < 1f || playerTransform is null)
+            {
+                return;
+            }
+
+            lastCheckAt = Time.time;
+
+            foreach (var objectTuple in objects)
+            {
+                if (Vector3.Distance(playerTransform.position, objectTuple.Item1.position) < objectDistanceThreshold)
+                {
+                    AddMember(objectTuple);
+                }
+                else
+                {
+                    RemoveMember(objectTuple);
+                }
+            }
         }
 
         public void SetPlayer(Transform player)
@@ -35,14 +55,15 @@ namespace Core.Camera
             var playerForward = new GameObject("PlayerForward");
             playerForward.transform.position = player.position;
             playerForward.transform.parent = player;
-            targetGroup.AddMember(playerForward.transform, CameraTargetGroupTag.Player().Weight, CameraTargetGroupTag.Player().Radius);
+            targetGroup.AddMember(playerForward.transform, CameraTargetGroupTag.Player().Weight,
+                CameraTargetGroupTag.Player().Radius);
             playerForwardObject = playerForward;
             playerTransform = player;
         }
 
         public void UpdatePlayerForward(Vector3 forward, float distance)
         {
-            if (playerForwardObject == null)
+            if (playerForwardObject is null)
             {
                 return;
             }
@@ -52,12 +73,37 @@ namespace Core.Camera
 
         public void AddTarget(Transform target, CameraTargetGroupTag targetGroupTag)
         {
-            targetGroup.AddMember(target, targetGroupTag.Weight, targetGroupTag.Radius);
+            if (!objects.Contains((target, targetGroupTag)))
+            {
+                objects.Add((target, targetGroupTag));
+            }
         }
-        
+
         public void RemoveTarget(Transform target)
         {
-            targetGroup.RemoveMember(target);
+            var index = objects.FindIndex(tuple => tuple.Item1 == target);
+            if (index > -1 && index < objects.Count)
+            {
+                objects.RemoveAt(index);
+            }
+        }
+
+        private void AddMember((Transform, CameraTargetGroupTag) item)
+        {
+            var find = targetGroup.FindMember(item.Item1) != -1;
+            if (find)
+            {
+                return;
+            }
+
+            var tags = item.Item2;
+            targetGroup.AddMember(item.Item1, tags.Weight, tags.Radius);
+        }
+
+        private void RemoveMember((Transform, CameraTargetGroupTag) item)
+        {
+            // removeは内部でcheckがあるのでここでは`find`をしない
+            targetGroup.RemoveMember(item.Item1);
         }
     }
 }
