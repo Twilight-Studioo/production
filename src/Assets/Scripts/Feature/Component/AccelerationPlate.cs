023e4c3b29ad6a066daab9078b926926a595b6b4
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-10-06 20:12:46 +0900

    加速板仮実装

---
+++ src/Assets/Scripts/Feature/Component/AccelerationPlate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Feature.Component
+{
+    public class AccelerationPlate : MonoBehaviour
+    {
+        [SerializeField] private float acceleration = 0.5f;
+        private bool OnAccelerationPlate = false;
+
+        private void OnCollisionStay(Collision other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                OnAccelerationPlate = true;
+                other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(acceleration,0,0),ForceMode.VelocityChange);
+            }
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                OnAccelerationPlate = false;
+            }
+        }
+    }
+}
