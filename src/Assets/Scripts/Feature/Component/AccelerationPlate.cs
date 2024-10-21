c58e16d13aae5e7e8211e729aab620753a7110aa
Hayato222 <g2227070@st.t-kougei.ac.jp>
2024-10-21 21:00:09 +0900

    加速方向を選択を追加

--- src/Assets/Scripts/Feature/Component/AccelerationPlate.cs
+++ src/Assets/Scripts/Feature/Component/AccelerationPlate.cs
@@ -10,22 +10,34 @@ namespace Feature.Component
     {
         [SerializeField] private float acceleration = 0.5f;
         [SerializeField] private float escapeSpeed = 20f;
+        [SerializeField] private bool moveRight = true;
         private readonly Vector3 directionMovement = new(1, 0, 0);
 
         private void OnCollisionExit(Collision other)
         {
             //if (other.gameObject.CompareTag("Player"))
+            if(moveRight)
             {
                 other.gameObject.GetComponent<Rigidbody>()?.AddForce(directionMovement * escapeSpeed);
+            }
+            else if(!moveRight)
+            {
+                other.gameObject.GetComponent<Rigidbody>()?.AddForce(-directionMovement * escapeSpeed);
             }
         }
 
         private void OnCollisionStay(Collision other)
         {
             //if (other.gameObject.CompareTag("Player"))
+            if(moveRight)
             {
                 other.gameObject.GetComponent<Rigidbody>()?
                     .MovePosition(other.transform.position + directionMovement * acceleration * Time.deltaTime);
+            }
+            else if(!moveRight)
+            {
+                other.gameObject.GetComponent<Rigidbody>()?
+                    .MovePosition(other.transform.position + -directionMovement * acceleration * Time.deltaTime);
             }
         }
     }
