9f8a21e11d76ab23aa30a1699b3327387c449d99
yuhi yamane <bird9.yuhi@gmail.com>
2024-11-05 13:37:30 +0900

    :rocket: perf: オプション画面impl

--- src/Assets/Scripts/Main/Controller/GameNavigation/OptionScreen.cs
+++ src/Assets/Scripts/Main/Controller/GameNavigation/OptionScreen.cs
@@ -1,5 +1,8 @@
 #region
 
+using System;
+using TMPro;
+using UniRx;
 using UnityEngine;
 
 #endregion
@@ -11,6 +14,35 @@ namespace Main.Controller.GameNavigation
     /// </summary>
     public class OptionScreen : ScreenProtocol
     {
+        [SerializeField] private TextMeshProUGUI volumesText;
+        [SerializeField] private TextMeshProUGUI controlsText;
+        [SerializeField] private TextMeshProUGUI backText;
+
+        
+        private readonly IReactiveProperty<Navi> currentNavi = new ReactiveProperty<Navi>();
+
+        private IDisposable disposable;
+        
+        public override void OnShow()
+        {
+            base.OnShow();
+            disposable = currentNavi.Subscribe(OnNaviChanged);
+            currentNavi.Value = Navi.Volumes;
+        }
+
+        public override void OnHide()
+        {
+            base.OnHide();
+            disposable?.Dispose();
+        }
+
+        private void OnNaviChanged(Navi navi)
+        {
+            volumesText.color = navi == Navi.Volumes ? Color.white : Color.HSVToRGB(0.6f, 0.2f, 0.6f);
+            controlsText.color = navi == Navi.Controls ? Color.white : Color.HSVToRGB(0.6f, 0.2f, 0.6f);
+            backText.color = navi == Navi.Back ? Color.white : Color.HSVToRGB(0.6f, 0.2f, 0.6f);
+        }
+        
         protected override void OnCancel()
         {
             Controller.PopBackstack();
@@ -18,12 +50,56 @@ namespace Main.Controller.GameNavigation
 
         protected override void OnNavigation(Vector2 value)
         {
-            // TODO: Implement navigation
+            var navi = currentNavi.Value;
+            if (value.y > 0)
+            {
+                switch (navi)
+                {
+                    case Navi.Controls:
+                        navi = Navi.Volumes;
+                        break;
+                    case Navi.Back:
+                        navi = Navi.Controls;
+                        break;
+                }
+            }
+            else if (value.y < 0)
+            {
+                switch (navi)
+                {
+                    case Navi.Volumes:
+                        navi = Navi.Controls;
+                        break;
+                    case Navi.Controls:
+                        navi = Navi.Back;
+                        break;
+                }
+            }
+            
+            currentNavi.Value = navi;
         }
 
         protected override void OnClick()
         {
-            Controller.Navigate(Navigation.Volumes);
+            switch (currentNavi.Value)
+            {
+                case Navi.Volumes:
+                    Controller.Navigate(Navigation.Volumes);
+                    break;
+                case Navi.Controls:
+                    Controller.Navigate(Navigation.Controls);
+                    break;
+                case Navi.Back:
+                    Controller.PopBackstack();
+                    break;
+            }
+        }
+        
+        private enum Navi
+        {
+            Volumes,
+            Controls,
+            Back,
         }
     }
 }
