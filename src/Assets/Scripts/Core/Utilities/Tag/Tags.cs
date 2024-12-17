4dcd89386701cff5406eafdc41982b892d526157
yuhi yamane <bird9.yuhi@gmail.com>
2024-12-17 12:25:22 +0900

    :bug: fix: SwapItemタグ関連の仕様変更対応
    ITagインターフェースを導入し、SwapItemタグ判定をリファクタリング。Prefabとスクリプトの整合性を修正。

---
+++ src/Assets/Scripts/Core/Utilities/Tag/Tags.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using UnityEngine;
+
+namespace Core.Utilities.Tag
+{
+    public static class TagUtility
+    {
+        public static bool HasTag<T>(this Component obj) where T : ITag
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+            return obj.GetComponents<ITag>().Any(tag => tag is T t && tag.TagName == t.TagName);
+        }
+        public static bool HasTag(this Component obj, string tagName)
+        {
+            return obj.GetComponents<ITag>().Any(tag => tag.TagName == tagName);
+        }
+
+        // 複数のタグを取得する
+        public static string[] GetTags(this Component obj)
+        {
+            return obj.GetComponents<ITag>().Select(tag => tag.TagName).ToArray();
+        }
+    }
+}
