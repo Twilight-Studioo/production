03c19da8b94ac3fca4f21d81fb461a99788bae16
yuhi yamane <bird9.yuhi@gmail.com>
2025-01-07 14:50:49 +0900

    delete folder

--- src/Packages/DynamicActFlow-main/Assets/DynamicActFlow/Runtime/Core/Action/Ref.cs
+++
@@ -1,7 +0,0 @@
-namespace DynamicActFlow.Runtime.Core.Action
-{
-    public abstract class Ref
-    {
-        internal abstract void SetParam(string key, object param);
-    }
-}
--- src/Packages/DynamicActFlow-main/Assets/DynamicActFlow/Runtime/Core/ActionName.cs
+++
@@ -1,8 +0,0 @@
-namespace DynamicActFlow.Runtime.Core
-{
-    public static class ActionName
-    {
-        internal const string Wait = "Wait";
-        internal const string InfinityWait = "InfinityWait";
-    }
-}
