bf11807991888b56415ddffd5f3395a438a68ec6
yuhi yamane <bird9.yuhi@gmail.com>
2025-01-13 14:31:26 +0900

    :recycle: refactor: シーン名プロパティを列挙型に変更
    シーン名を文字列から列挙型に変更し、型安全性を向上。関連するフィールドや条件分岐も更新。

--- src/Assets/Scripts/Main/Scene/FixedLoadSceneArea.cs
+++ src/Assets/Scripts/Main/Scene/FixedLoadSceneArea.cs
@@ -7,31 +7,13 @@ namespace Main.Scene
     [RequireComponent(typeof(Collider))]
     public class FixedLoadSceneArea : MonoBehaviour
     {
-        [SerializeField] private string sceneName;
-        private Generated.Scene? scene;
-
-        private void Awake()
-        {
-            if (string.IsNullOrEmpty(sceneName))
-            {
-                Debug.LogError("SceneName is empty.");
-            }
-
-            if (Enum.TryParse<Generated.Scene>(sceneName, out var result))
-            {
-                scene = result;
-            }
-            else
-            {
-                Debug.LogError($"SceneName is invalid. SceneName: {sceneName}");
-            }
-        }
+        [SerializeField] private Generated.Scene sceneName;
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Player") && scene.HasValue)
+            if (other.CompareTag("Player"))
             {
-                SceneLoaderFeatures.GetSceneLoader(scene.Value, null).Bind(RootInstance.Shared).Load();
+                SceneLoaderFeatures.GetSceneLoader(sceneName, null).Bind(RootInstance.Shared).Load();
             }
         }
     }
